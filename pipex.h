3b9156c09ffa8e747a8aefd744217bf4b8f3fad4
Mohammed Abid <mohabid@c4r6p6.1337.ma>
2025-01-02 04:05:49 +0100

    put this here for now

--- pipex.h
+++ pipex.h
@@ -6,14 +6,14 @@
 /*   By: mohabid <mohabid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 04:00:18 by mohabid           #+#    #+#             */
-/*   Updated: 2025/01/02 04:04:11 by mohabid          ###   ########.fr       */
+/*   Updated: 2025/01/02 04:05:42 by mohabid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "../libft.h"
+# include "libft/libft.h"
 char	**return_path(char *env[]);
 char	*create_full_path(char *path, char *command);
 char	*path_found(char *command, char *env[]);
